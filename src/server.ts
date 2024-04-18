7671656d80fab550eb690aa656bb4aeec2c2480e
Alexander Kolomeets <akolomeets@dataeast.com>
2024-04-18 16:39:59 +0700

    fix ejs

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,13 @@ import chalk from "chalk";
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
+import path from "path";
 
 import { Application } from "express";
 import { apiDayRoutes } from "./routes/api-day-routes";
 import { apiDeptRoutes } from "./routes/api-dept-routes";
 import { apiTaskRoutes } from "./routes/api-task-routes";
 import { apiUserRoutes } from "./routes/api-user-routes";
-import { createPath } from "./utils/create-path";
 
 const errorMsg = chalk.bgKeyword("white").redBright;
 const successMsg = chalk.bgKeyword("green").white;
@@ -19,7 +19,7 @@ const successMsg = chalk.bgKeyword("green").white;
 const app: Application = express();
 app.use(cors());
 app.set("view engine", "ejs");
-app.use(express.static("styles"));
+app.use(express.static("src/views/styles"));
 //app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
@@ -31,9 +31,10 @@ const PORT = process.env.PORT || 5000;
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms")
 );
+app.set("views", path.join(__dirname, "views"));
 app.get("/", (req, res) => {
   const title = "Home";
-  res.render(createPath("index"), { title });
+  res.status(200).render("index", { title });
 });
 app.use(apiTaskRoutes);
 app.use(apiDayRoutes);
@@ -41,5 +42,5 @@ app.use(apiDeptRoutes);
 app.use(apiUserRoutes);
 app.use((req, res) => {
   const title = "Error Page";
-  res.status(404).render(createPath("error"), { title });
+  res.status(404).render("error", { title });
 });
--- src/utils/create-path.ts
+++
@@ -1,3 +0,0 @@
-import path from "path";
-export const createPath = (page: string) =>
-  path.resolve(__dirname, "../views", `${page}.ejs`);
